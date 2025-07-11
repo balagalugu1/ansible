9ea4ea1900b06e695e7dd86f07613d9cc6a1b5fb
unknown <gbnarasimha18@gmail.com>
2025-07-11 16:07:19 +0530

    redis server

--- redis.tf
+++ redis.tf
@@ -1,21 +1,16 @@
-data "aws_ami" "redis_ami" {
-     most_recent      = true
-     name_regex       = "^DevSecOps"
-     owners           = ["721834156908"]
-}
-
-
-resource "aws_instance" "redis-server" {
-  ami                         = data.aws_ami.my_ami.id
+resource "aws_instance" "public" {
+  #   count = length(var.az)
+  #count = local.new_environment == "dev" ? 3 : 1
+  ami   = data.aws_ami.my_ami.id
+  #ami = "ami-0d857ff0f5fc4e03b"
+  #ami = "${data.aws_ami.my_ami.id}"
   instance_type               = "t2.medium"
   key_name                    = var.key_name
-  subnet_id                   = aws_subnet.public-subnets.0.id
+  subnet_id                   = aws_subnet.subnet1-public.0.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
   tags = {
-    Name              = "Redis-Server"
-    Terraform-Managed = "Yes"
-    Env               = local.new_environment
-    ProjectID         = local.projid
+    Name = "${var.vpc_name}-redis_server-${count.index + 1}"
+    Env  = var.env
   }
 }
